216b43c396292f2d2da92436b2c5306cb5fce7b5
Euston <raja.r184@gmail.com>
2015-12-16 19:55:13 +0000

    Completed counter ratings and summoner kda rankings

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import requests
 import sys
 import os
 import bs4
+import re
 from pprint import pprint
 config_file = 'config.json'
 if not os.path.isfile(config_file):
@@ -40,7 +41,7 @@ def get_soup(stuff, argtings):
     return soup
 
 
-def do_thing(name):
+def match_data(name):
     dic = get_data(
         '%s/api/lol/euw/v1.4/summoner/by-name/%s?api_key=%s', (url,
                                                                name,
@@ -65,6 +66,7 @@ def do_thing(name):
             (global_url, region, item['spell2Id'], api_key))
         spell2 = dic['name']
         skim[item['summonerName']] = {'id': item['summonerId'],
+                                      'champ_id': item['championId'],
                                       'champ': champ_name,
                                       'spells': [spell1,
                                                  spell2],
@@ -73,29 +75,26 @@ def do_thing(name):
 
 
 def lane(play_info):
+    posfile = open('champ_pos.json', 'r')
+    pos = json.load(posfile)
+    posfile.close()
     positions = {}
-    remainder = ['Top', 'Mid', 'Bottom', 'Bottom', 'Jungle']
+    remainder = ['Top', 'Mid', 'Bottom', 'Bottom', 'Jungler']
     champs = []
     for item in play_info:
         if item['spells'][0] == 'Smite' or item['spells'][1] == 'Smite':  # NOQA
-            positions[item['champ']] = 'Jungle'
-            remainder.remove('Jungle')
+            positions[item['champ']] = 'Jungler'
+            remainder.remove('Jungler')
             continue
         champs.append(item['champ'])
 
     rejects = {}
 
     for cena in champs:
-        soup = get_soup(
-            'http://www.lolcounter.com/champions/%s', (cena))
-        elems = soup.select('.block-tabs')
-        possibles = elems[0].getText()
-        possibles = possibles.strip('\n')
-        possibles = possibles.split('\n')
-        possibles.remove('All')
-        possibles.remove('General')
-        if 'Jungler' in possibles:
-            possibles.remove('Jungler')
+        possibles = pos[cena]
+        if 'Jungler' not in remainder:
+            if 'Jungler' in possibles:
+                possibles.remove('Jungler')
         if len(possibles) == 1:
             positions[cena] = possibles[0]
             remainder.remove(possibles[0])
@@ -104,24 +103,34 @@ def lane(play_info):
     count = 0
     while remainder:
         count += 1
-        if count > 5:
+        if count > 30:
             break
+        # What is left
         for key in rejects:
             for item in rejects[key]:
                 if item not in remainder:
                     rejects[key].remove(item)
             if len(rejects[key]) == 1:
                 positions[key] = rejects[key][0]
-                del rejects[key]
                 remainder.remove(rejects[key][0])
-    if rejects:
+        # Only they can x lane
+        current_count = {}
+        for key in rejects:
+            for item in current_count:
+                if item in current_count.keys():
+                    current_count[item] += 1
+                else:
+                    current_count
+        for key in current_count:
+            if current_count[key] == 1:
+                for item in rejects:
+                    if key in item:
+                        positions[key] = item
+                        remainder.remove(item)
+    if remainder:
         for key in rejects:
             positions[key] = rejects[key]
     return positions
-
-
-def strength(champ1, champ2):
-    pass
 
 
 def teams(fams):
@@ -135,8 +144,72 @@ def teams(fams):
             bully.append(fams[key])
     return friend, bully
 
-skim = tester()
 
-t1, t2 = teams(skim)
+def player_info(sid, cid):
+    c = {}
+    data = get_data(
+        '%s/api/lol/%s/v1.3/stats/by-summoner/%s/ranked?api_key=%s',
+        (url, region, sid, api_key))
+    c = data['champions']
+    stats = {}
+    for item in c:
+        if item['id'] == cid:
+            stats = item['stats']
+            break
+    if not stats:
+        return ['Cherry Popped']
+    else:
+        return ['%.2f' % (stats['totalChampionKills']/stats['totalDeathsPerSession']),  # NOQA
+            '%.2f' % (stats['totalAssists']/stats['totalSessionsPlayed']),
+            '%.2f' % (stats['totalSessionsWon']/stats['totalSessionsLost']),
+            str(stats['totalSessionsPlayed'])]
 
-pprint(lane(t1))
+
+def counter_rating(friend, bully):
+    soup = get_soup('http://www.lolcounter.com/champions/%s/weak',
+                    (bully))
+    weak = soup.select('.weak-block .name')
+    for item in weak:
+        if item.getText() == friend:
+            return 'green'
+    soup = get_soup('http://www.lolcounter.com/champions/%s/strong',
+                    (bully))
+    strong = soup.select('.weak-block .name')
+    for item in strong:
+        if item.getText() == friend:
+            return 'red'
+    return 'yellow'
+
+
+def counter_tips(friend, bully):
+    soup = get_soup(
+        'http://www.lolcounter.com/champions/%s/%s',
+        (bully, friend))
+    data = soup.select('.tip-block ._tip')
+    gentips = []
+    for item in data:
+        gentips.append(item.getText())
+    soup = get_soup(
+        'http://www.lolcounter.com/tips/%s/%s',
+        (bully, friend))
+    data = soup.select('._tip')
+    spectips = []
+    for item in data:
+        spectips.append(item.getText())
+    return [gentips, spectips]
+
+
+def matchup(friendlies, bullies):
+    pass
+
+
+def run():
+    skim = tester()
+    friendlies, bullies = teams(skim)
+    friendlylane = lane(friendlies)
+    bullylane = lane(bullies)
+    print(friendlylane)
+
+
+if __name__ == "__main__":
+    run()
