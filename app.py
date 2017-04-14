8855f03cca620be59ba5a3ea56abfe3f9086271a
Euston <raja.r184@gmail.com>
2017-04-14 21:40:13 +0100

    completed backend

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request
 from forms import SummonerForm
 import backend
-import os
 import simplejson as json
+import os
 app = Flask(__name__)
 app.config.from_object('config')
 
@@ -19,22 +19,31 @@ def fucked(reason):
 
 @app.route('/viewgame', methods=['POST'])
 def viewgame():
+    test = True
     name = request.form['name']
     region = request.form['region']
     key = os.environ.get('rito_api_key')
-    config = backend.getConfig(name, region, key)
-    if not config:
-        fucked("Couldn't find Summoner: %s in %s" % (name, region))
+    if not test:
+        config = backend.get_config(name, region, key)
+        if not config:
+            return fucked("Couldn't find Summoner: %s in %s" % (name, region))
 
-    match = backend.get_match(config)
-    if not match:
-        return fucked("%s is not in a game" % (name))
+        match = backend.get_match(config, test=True)
+        if not match:
+            return fucked("%s is not in a game" % (name))
 
-    santised_data = backend.load_match()
+        friend, bully = backend.load_match(match[0], match[1], config)
+    else:
+        filename = open('refined_testdata.json')
+        x = json.load(filename)
+        filename.lose()
+        friend, bully = x['Friends'], x['Bullies']
 
     return render_template('results.html',
                            title='Name Page',
-                           name = name)
+                           name = name,
+                           friend = friend,
+                           bully = bully)
 
 
 if __name__=="__main__":
--- backend.py
+++ backend.py
@@ -1,5 +1,9 @@
 import simplejson as json
+from pprint import pprint
 import requests
+import time
+import re
+
 
 endpoints = {
     'BR': 'BR1',
@@ -16,8 +20,147 @@ endpoints = {
     'PBE': 'PBE1'
 }
 
-def load_match(config):
-    pass
+
+def get_stats(playalist, config, results):
+    for item in playalist:
+        if str(item['summonerId']) in results.keys():
+            item['league'] = results[str(item['summonerId'])]['tier'] + ' ' + results[str(item['summonerId'])]['entries'][0]['division']  # NOQA
+        else:
+            item['league'] = 'True Wood'
+        item['champMastery'] = get_champ_mastery(item['summonerId'],
+                                                 item['championId'],
+                                                 config)
+    return playalist
+
+
+def parse_runes(desc, count):
+    regex = r'([+-]\d+\.?\d*\%?) ([\(\)\s\w]+)'
+    s = re.findall(regex, desc)
+    output = ""
+    for item in s:
+        percent = False
+        if item[0].endswith("%"):
+            percent = True
+            num = item[0].strip('%')
+        else:
+            num = item[0]
+        x = float(num) * count
+        if x >= 0:
+            output += "+"
+        output += str(x)
+        if percent:
+            output += "%"
+        output += " "
+        output += item[1]
+    return output
+
+
+
+def sanitisedat(playas, runedata, masterydata, spelldata, champdata, config, friend):  # NOQA
+    ddragon = 'http://ddragon.leagueoflegends.com/cdn/%s/img' % config['version']  # NOQA
+    output = []
+    for item in playas:
+        entry = {}
+        entry['champMastery'] = item['champMastery']['championLevel']
+        entry['league'] = item['league']
+        entry['summoner'] = item['summonerName']
+        entry['masteries'] = []
+        for m in item['masteries']:
+            n = {}
+            dat = masterydata['data'][str(m['masteryId'])]
+            n['name'] = dat['name']
+            n['desc'] = dat['description'][m['rank']-1]
+            n['image'] = '%s/mastery/%s' % (ddragon, dat['image']['full'])
+            entry['masteries'].append(n)
+        entry['runes'] = []
+        for r in item['runes']:
+            count = r['count']
+            x = {}
+            dat = runedata['data'][str(r['runeId'])]
+            x['image'] = '%s/rune/%s' % (ddragon, dat['image']['full'])
+            x['desc'] = parse_runes(dat['sanitizedDescription'], count)
+            entry['runes'].append(x)
+        entry['Dkey'] = spelldata[str(item['spell1Id'])]
+        entry['Fkey'] = spelldata[str(item['spell2Id'])]
+        # Champion Data
+        c = champdata[str(item['championId'])]
+        entry['champ'] = c['name']
+        entry['key'] = c['key']
+        entry['image'] = "%s/champion/%s" % (ddragon, c['image'])
+        if friend:
+            entry['tips'] = c['allytips']
+        else:
+            entry['tips'] = c['enemytips']
+        entry['passive'] = {
+            'image': "%s/passive/%s" % (ddragon, c['passive']['image']['full']),
+            'desc' : c['passive']['sanitizedDescription']
+        }
+
+        entry['spells'] = []
+        # Still misssing shit on ritos end
+        for spell in c['spells']:
+            x = {
+                'image' : "%s/spells/%s" % (ddragon, spell['image']),
+                'cooldown' :spell['cooldownBurn'],
+                'desc' : spell['sanitizedDescription']
+
+            }
+            entry['spells'].append(x)
+
+        output.append(entry)
+
+    return output
+
+
+
+def fix_your_champs_rito(champdata):
+    output = {}
+    for item in champdata['data'].values():
+        entry = {
+            'name': item['name'],
+            'image': item['image']['full'],
+            'title': item['title'],
+            'enemytips': item['enemytips'],
+            'allytips': item['allytips'],
+            'passive': item['passive'],
+            'spells': item['spells'],
+            'key': item['key']
+        }
+        output[str(item['id'])] = entry
+    return output
+
+
+
+def fix_your_spells_rito(spelldata):
+    output = {}
+    for item in spelldata['data'].values():
+        entry = {
+            'image': item['image']['full'],
+            'name': item['name'],
+            'desc': item['sanitizedDescription']
+        }
+        output[str(item['id'])] = entry
+    return output
+
+
+def load_match(friendlies, bullies, config):
+    friendly_list = []
+    bully_list = []
+    for item in friendlies:
+        friendly_list.append(item['summonerId'])
+    for item in bullies:
+        bully_list.append(item['summonerId'])
+    results = get_league(friendly_list + bully_list, config)
+    friendlies = get_stats(friendlies, config, results)
+    bullies = get_stats(bullies, config, results)
+    masterydata = get_masteries(config)
+    runedata = get_runes(config)
+    spelldata = fix_your_spells_rito(get_spells(config))
+    champdata = fix_your_champs_rito(get_champ(config))
+    foutput = sanitisedat(friendlies, runedata, masterydata, spelldata, champdata, config, True)
+    boutput = sanitisedat(bullies, runedata, masterydata, spelldata, champdata, config, True)
+
+    return foutput, boutput
 
 
 def get_version(config):
@@ -37,6 +180,7 @@ def get_config(new_name, region, apikey):
     if not sumdata:
         return False
     con = {}
+    con['global'] = 'https://global.api.riotgames.com'
     con['api_key'] = apikey
     con['summoner'] = new_name
     con['region'] = region.lower()
@@ -44,16 +188,22 @@ def get_config(new_name, region, apikey):
     con['url'] = 'https://%s.api.pvp.net' % region.lower()
     con['id'] = sumdata[
         ''.join(c.lower() for c in new_name if not c.isspace())]['id']
-    con['version'] = get_version()
+    con['version'] = get_version(con)
     return con
 
 
-def get_data(url, argtings):
+def get_data(url, argtings, recursion = 5):
     """Retrieves data from url using selected arguments"""
+    if recursion == 0:
+        print("SYSTEM ERROR")
+        return None
     response = requests.get(url % argtings)
-    print(response.text)
     if response.status_code == 404 or response.status_code == 403:
         return None
+    if response.status_code == 429:
+        for i in range(10):
+            time.sleep(1)
+        return get_data(url, argtings, recursion=recursion-1)
     try:
         return json.loads(response.text)
     except json.scanner.JSONDecodeError:
@@ -116,12 +266,11 @@ def get_champ_mastery(summoner, champ, config):
     return data
 
 
-def get_champ(champ_id, config):
+def get_champ(config):
     """retrieves static champion data"""
-    data = get_data('%s/api/lol/static-data/%s/v1.2/champion/%d?champData=passive,image,spells&api_key=%s',  # NOQA
+    data = get_data('%s/api/lol/static-data/%s/v1.2/champion?champData=allytips,enemytips,passive,image,spells&api_key=%s',  # NOQA
                     (config['global'],
                      config['region'],
-                     champ_id,
                      config['api_key']))
     return data
 
@@ -137,18 +286,33 @@ def get_masteries(config):
 
 def get_runes(config):
     """retrieve static rune data"""
-    data = get_data('%s/api/lol/static-data/%s/v1.2/rune?runeListData=image&api_key=%s',  # NOQA
+    data = get_data('%s/api/lol/static-data/%s/v1.2/rune?runeListData=image,sanitizedDescription,stats,tags&api_key=%s',  # NOQA
                     (config['global'],
                      config['region'],
                      config['api_key']))
     return data
 
 
-def get_spells(sid, config):
+def get_spells(config):
     """retrieve static spell data"""
-    data = get_data('%s/api/lol/static-data/%s/v1.2/summoner-spell/%d?api_key=%s',  # NOQA
+    data = get_data('%s/api/lol/static-data/%s/v1.2/summoner-spell?spellData=image,sanitizedDescription&api_key=%s',  # NOQA
                     (config['global'],
                      config['region'],
-                     sid,
                      config['api_key']))
     return data
+
+
+if __name__ == "__main__":
+    import os
+    key = os.environ.get('rito_api_key')
+    config = get_config("WoodDivisionSupp", "EUW", key)
+    match = get_match(config, test=True)
+    dats = load_match(match[0], match[1], config)
+    x = {
+        "Friends": dats[0],
+        "Bullies": dats[1]
+    }
+    filename = open("refined_testdata.json", "w")
+    json.dump(x, filename)
+    filename.close()
+    #parse_runes("+4.5% attack speed", 9)
